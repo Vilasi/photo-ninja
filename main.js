30340e4605e19eab9985733c370b7ab4fbb8bab1
Vilasi <VilasiCoding@gmail.com>
2020-07-21 15:53:48 -0400

    Completed Site

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
 
     $('.materialboxed').materialbox();
 
+    $('.parallax').parallax();
+
+    $('.tabs').tabs();
+
+    /* function datepicker() takes in an object as a parameter */
+    $('.datepicker').datepicker({
+        disableWeekends: true
+    });
+
+    $('.tooltipped').tooltip();
+
+    $('.scrollspy').scrollSpy();
+
 
 
 
